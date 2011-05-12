dd83fd0fc3375448b52763787a27a24b9bfb1ad0
Colin Bartlett <dev+cbartlett@hashrocket.com>
2011-05-12 08:56:35 -0400

    Initial idea for rake leaves

---
+++ Rakefile
@@ -0,0 +1,5 @@
+require File.dirname(__FILE__) + '/leaves.rb'
+
+task :leaves do
+  Leaves.find
+end
---
+++ leaves.rb
@@ -0,0 +1,18 @@
+module Leaves
+  extend self
+
+  PATTERNS = ['show me the page', 'debug']
+
+  def status
+    `git status --porcelain`.split("\n")
+  end
+
+  def files
+    status.reject { |f| f.match(/^ D/) }.map { |f| f[3..f.size] }
+  end
+
+  def find
+    PATTERNS.each { |p| system "grep '#{p}' #{files.join(' ')}" }
+  end
+
+end
